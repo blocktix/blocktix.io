cbd280533c1738deb85035c861d5a95125b018cc
Strzhyhotski Artem <aa.temkin@gmail.com>
2017-10-31 13:20:52 +0300

    remove unused JS method

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,3 @@
-var isInViewport = function(element) {
-  var rect = element.getBoundingClientRect();
-  var html = document.documentElement;
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || html.clientHeight) &&
-    rect.right <= (window.innerWidth || html.clientWidth)
-  );
-}
 
 var debounce = function(func, wait, immediate) {
   var timeout;
