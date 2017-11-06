9f06258bf55660e412cc19751aee08795640e9d3
Strzhyhotski Artem <aa.temkin@gmail.com>
2017-11-06 15:22:53 +0300

    fixes according client response

--- src/js/index.js
+++ src/js/index.js
@@ -53,54 +53,6 @@ document.addEventListener("DOMContentLoaded", function() {
   }, 25);
   window.addEventListener('scroll', scroll_watch);
 
-  // var slider        = document.querySelector('.team_slider');
-  // var dot_count     = slider.querySelectorAll('.js_slide').length;
-  // var dot_container = document.querySelector('ul.navigation');
-  // var dot_list_item = document.createElement('li');
-
-  // function handleDotEvent(e) {
-  //     if (e.type === 'before.lory.init') {
-  //       for (var i = 0, len = dot_count; i < len; i++) {
-  //         var clone = dot_list_item.cloneNode();
-  //         dot_container.appendChild(clone);
-  //       }
-  //       dot_container.childNodes[0].classList.add('active');
-  //     }
-  //     if (e.type === 'after.lory.init') {
-  //       for (var i = 0, len = dot_count; i < len; i++) {
-  //         dot_container.childNodes[i].addEventListener('click', function(e) {
-  //           var index = Array.prototype.indexOf.call(dot_container.childNodes, e.target);
-  //           dot_navigation_slider.slideTo(index);
-  //         });
-  //       }
-  //     }
-  //     if (e.type === 'after.lory.slide') {
-  //       for (var i = 0, len = dot_container.childNodes.length; i < len; i++) {
-  //         dot_container.childNodes[i].classList.remove('active');
-  //       }
-
-  //       if (dot_container.childNodes[e.detail.currentSlide - 1]){
-  //         dot_container.childNodes[e.detail.currentSlide - 1].classList.add('active');
-  //       }
-  //     }
-  //     if (e.type === 'on.lory.resize') {
-  //         for (var i = 0, len = dot_container.childNodes.length; i < len; i++) {
-  //             dot_container.childNodes[i].classList.remove('active');
-  //         }
-  //         dot_container.childNodes[0].classList.add('active');
-  //     }
-  // }
-  // slider.addEventListener('before.lory.init', handleDotEvent);
-  // slider.addEventListener('after.lory.init', handleDotEvent);
-  // slider.addEventListener('after.lory.slide', handleDotEvent);
-  // slider.addEventListener('on.lory.resize', handleDotEvent);
-
-
-  // var dot_navigation_slider = lory(slider, {
-  //     infinite: 1,
-  //     slidesToScroll: 1
-  // });
-
   initSlider(document.querySelector('.cards'))
   window.onresize = function(){
     initSlider(document.querySelector('.cards'))
@@ -110,9 +62,9 @@ document.addEventListener("DOMContentLoaded", function() {
   var questions     = faq_container.querySelectorAll('.question')
   var questions_cnt = questions.length;
   for(var i = 0; i < questions_cnt; i++){
-    questions[i].addEventListener('click', function(e){
+    questions[i].querySelector('.title').addEventListener('click', function(e){
       e.preventDefault();
-      var self = this;
+      var self = this.parentNode;
       for(var x = 0; x < questions_cnt; x++){
         if (questions[x] != self){
           questions[x].classList.remove('active')
